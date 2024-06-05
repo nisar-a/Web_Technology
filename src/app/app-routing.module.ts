b292800b3caa942c6f19c4f9fa5d6aaac99456d7
Nisar.A <124667527+nisar-a@users.noreply.github.com>
2024-06-05 10:27:56 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{path:'about',component:AboutComponent},
+{path:'dashboard',component:DashboardComponent},
+{path:'home',component:HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
